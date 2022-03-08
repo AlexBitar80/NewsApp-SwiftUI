a494370bad3c7962a0780b329a6a957ba0ef6d42
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-03-08 19:56:01 -0300

    displaying data coming from the API

--- newsApp-SwiffUI/ContentView.swift
+++ newsApp-SwiffUI/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {
-            List(posts) { post in
-                Text(post.title)
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
             }
+            .listStyle(.plain)
             .navigationTitle("Noticias do dia")
+        }
+        .onAppear {
+            self.networkManager.featchData()
         }
     }
 }
@@ -23,15 +32,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-struct Post: Identifiable {
-    let id: String
-    let title: String
-}
-
-let posts = [
-    Post(id: "1", title: "Noticia do dia 1"),
-    Post(id: "2", title: "Noticia do dia 2"),
-    Post(id: "3", title: "Noticia do dia 3"),
-    Post(id: "4", title: "Noticia do dia 4"),
-]
