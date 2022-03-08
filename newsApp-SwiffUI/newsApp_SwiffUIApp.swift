4143dc17e2c724041d7a82a34e3af6490d52cfdc
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-03-08 15:00:12 -0300

    Initial Commit

---
+++ newsApp-SwiffUI/newsApp_SwiffUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsApp_SwiffUIApp.swift
+//  newsApp-SwiffUI
+//
+//  Created by João Alexandre on 08/03/22.
+//
+
+import SwiftUI
+
+@main
+struct newsApp_SwiffUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
